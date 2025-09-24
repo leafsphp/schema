a9ebeca139217125140f6d122ce9caad806f00db
mychidarko <mickdd22@gmail.com>
2025-09-24 22:25:32 +0000

    feat: switch to sprout

---
+++ src/Commands/DatabaseCommands.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Leaf\Commands;
+
+class DatabaseCommands
+{
+    /**
+     * Return all commands
+     */
+    public static function commands()
+    {
+        return [
+            DatabaseDropCommand::class,
+            DatabaseMigrationCommand::class,
+            DatabaseResetCommand::class,
+            DatabaseRollbackCommand::class,
+            DatabaseSeedCommand::class,
+            DeleteSchemaCommand::class,
+            GenerateSchemaCommand::class,
+        ];
+    }
+}
